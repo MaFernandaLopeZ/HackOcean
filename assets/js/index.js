3ac1f0edccbf563ccaada860c147551187462603
MaFernandaLopeZ <maferlz265@gmail.com>
2022-05-14 20:31:55 -0500

    complemento

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+
+function startTime() {
+    hoy = new Date();
+    mes = (hoy.getMonth() + 1);
+    if (mes < 10) {
+        fecha = hoy.getDate() + '/0' + mes + '/' + hoy.getFullYear();
+    } else {
+        fecha = hoy.getDate() + '/' + (hoy.getMonth() + 1) + '/' + hoy.getFullYear();
+    }
+    h = hoy.getHours();
+    m = hoy.getMinutes();
+    s = hoy.getSeconds();
+    document.getElementById('reloj').innerHTML = fecha + " " + h + ":" + m + ":" + s;
+}
+window.onload = function () { startTime(); }
